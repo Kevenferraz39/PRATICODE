6d4845ea60a113334e6fc36c852ecddf168e1018
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-07 18:29:47 -0300

    atualizacao no arquivo alunos do crud e no css

--- view/crud/aluno/index.php
+++ view/crud/aluno/index.php
@@ -99,9 +99,9 @@
     <center>
         <div class="conteudo-tabela">
         <div class="top">
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
-                <path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path><path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path><path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"></path><path fill="#EEE" d="M24,30.9v4.2l-7.9-2.6L15.7,27h4l0.2,2.5L24,30.9z M19.1,17H24v-4h-9.1l0.7,12H24v-4h-4.6L19.1,17z"></path>
-                </svg><h3>Perguntas de HTML 5</h3>
+        <img src="../../_img/Logo-SemFundo.png"width="80" height="80">
+                
+                </svg><h3>Usuarios cadastrados</h3>
         </div>
            <table>
                 <thead>
@@ -128,8 +128,8 @@
                 </tbody>
             </table>
         </div><br>
-        <a href="cadastrar.php">Cadastrar Novo Aluno</a><br>
-        <a href="../../index.html">Voltar</a>
+        <a href="cadastrar.php" class="bt">Cadastrar Novo Aluno</a>
+        <a href="../../index.html" class="bt">Voltar</a>
     </center>
         <!--Start of Tawk.to Script-->
         <script src="JS/chat.js"></script>
