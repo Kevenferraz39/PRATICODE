797947515fd90c8a81f4c784201aff48c198dc41
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-08 21:40:35 -0300

    Update index.php

--- view/crud/exercicio/index.php
+++ view/crud/exercicio/index.php
@@ -1,15 +1,44 @@
+<?php 
+    include_once('../../../controller/crud/aluno/consultar.php');
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consultar Exercícios</title>
-</head>
+    <!-- css -->
+    <link rel="stylesheet" href="../../_css/Menu.css">
+    <link rel="stylesheet" href="../../_css/footer.css"> 
+    <link rel="stylesheet" href="../../_css/Login.css">
+    <link rel="stylesheet" href="../../_css/aluno.css">
+    <!-- fim-css -->
+    <link rel="shortcut icon" href="../../_img/logo.png" type="image/x-icon">
+    <title>Exercicios</title> 
+</head> 
 <body>
-    <a href="../index.html">Voltar</a>
-    <div>
-        <table>
-            <caption>Exercícios</caption>
+     <!-- Menu -->
+     <header class="Home">     
+    <a href="Menu.html">
+    <img src="../../_img/Logo-SemFundo.png" alt="foto" id="logo"/></a>
+        <div class="link">
+            <li class="focus"><b><a href="../../Menu.html">Exercicios</a></b></li>
+            <li class="linke"><b><a href="../index.html">CRUD</a></b></li>
+            <li class="linke"><b><a href="../../controller/login/sair.php">Sair</a></b></li>         
+        </div>
+</header>
+    <!-- fim do Menu -->
+    <!-- conteudo da pagina -->
+    <br><br><br><br><br>
+<div class="content">
+    <center>
+        <div id="container-conteudo-tabela">
+            <div class="top">
+                    <img src="../../_img/Logo-SemFundo.png">
+                    <h3>Exercicios cadastrados</h3>     
+            </div>
+            <div class="conteudo-tabela">
+            <table>
             <thead>
                 <tr>
                     <th>Id</th>
@@ -38,8 +67,68 @@
                 <?php } ?>
             </tbody>
         </table>
-        <br>
-        <a href="cadastrar.php">Cadastrar Novo Exercício    </a>
-    </div>
+            </div><br><br>
+                <a href="../index.html" class="btv">Voltar</a>
+                <a href="cadastrar.php" class="bt">Cadastrar Novo Exercício</a>
+        </div>
+    </center>
+        <!--Start of Tawk.to Script-->
+        <script src="JS/chat.js"></script>
+            <!--End of Tawk.to Script-->    
+</div>
+    <!--  fim do conteudo da pagina -->
+    <!--  footer -->
+    <footer>
+        <div class="footer-line"></div>
+        <div class="footer-wrapper">
+            <div class="footer-columns">
+                <section class="footer-logo">
+                    <a href="#">
+                        <img src="../../_img/LogoBrancaSemFundo.png" alt="logo">
+                    </a>
+                        <!-- More sections -->            
+            </div>
+            <div class="footer-bottom">
+                <div class="social-links">
+                    <ul>
+                        <li>
+                            <a href="#" title="Instagram">
+                                <img src="../../_img/Instagram.png" alt="imagem">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="gith">
+                                <img src="../../_img/github-logo-removebg-preview.png" alt="imagem">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="tiktok">
+                                <img src="../../_img/tiktok-removebg-preview.png" alt="imagem">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="linkedin">
+                                <img src="../../_img/icons8-linkedin-48.png" alt="imagem">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="email">
+                                <img src="../../_img/icons8-gmail-logo-48.png" alt="imagem">
+                            </a>
+                        </li>
+                        <!-- More social links -->
+                    </ul>
+                </div>
+                <small>
+                    &copy; Atheros Intelligence Ltd. <span id="year"></span>,
+                    All rights reseved
+                </small>
+            </div>
+        </div>
+    </footer>
+    <script>
+        document.getElementById('year').innerHTML = new Date().getFullYear();
+    </script>
+    <!--  fim do footer -->
 </body>
 </html>
