e67feee81c6121c941b6a4bcdc487b400be02bd3
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-08 21:39:37 -0300

    Update cadastro.php

--- controller/cadastro/cadastro.php
+++ controller/cadastro/cadastro.php
@@ -8,14 +8,14 @@
         $senhaConf = $_POST['senhaConf'];
 
         if ($senha === $senhaConf) {
-            include_once('../config/ClassConexao.php');
+            include_once('../../config/ClassConexao.php');
             $conectar = new Conexao();
             $conexao = $conectar->conectarBanco();
 
             $sqlInsert = "INSERT INTO aluno(nome, email, senha) VALUES ('$nome', '$email', '$senha')";
             $result = $conexao->query($sqlInsert);
 
-            header('Location: ../view/menu.html');
+            header('Location: ../../view/menu.html');
         } else {
             echo "Senha incorreta";
         }
