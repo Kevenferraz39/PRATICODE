2064e1c2d2d70fe1a7f5ef1b28289f98bfc2a300
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-09 21:43:48 -0300

    Create dropdown.js

---
+++ view/JS/dropdown.js
@@ -0,0 +1,14 @@
+//Quando o usuário clicar no botão, alterne entre ocultar e mostrar o conteúdo do menu suspenso
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  //Feche o menu suspenso se o usuário clicar fora dele
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
